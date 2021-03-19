a392615d23105993db7982e3ca660ade57e7347e
Zainul1996 <Zainul1996@gmail.com>
2021-03-19 15:31:19 +0800

    added requirements.txt and app.run for web app on port 8080

--- NoSQL/app.py
+++ NoSQL/app.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template
 from flask_pymongo import PyMongo
+
 app = Flask(__name__)
 
 # Establish connection to mongodb hosted on mongodb Atlas
-app.config['MONGO_URI'] = 'mongodb+srv://root:weijian!96@dbproject.bhdqc.mongodb.net/dbproject?retryWrites=true&w=majority'
+app.config[
+    "MONGO_URI"
+] = "mongodb+srv://root:weijian!96@dbproject.bhdqc.mongodb.net/dbproject?retryWrites=true&w=majority"
 mongo = PyMongo(app)
 
 # Declare the collection
@@ -13,7 +16,13 @@ products = mongo.db.products
 # def index():
 #     return render_template('posDashboard.html')
 
-@app.route('/', methods=['GET'])
+
+@app.route("/", methods=["GET"])
 def get_products():
-    all_products = products.find({},{"name":1, "price":1, "quantity":1})
-    return render_template('posDashboard.html', all_products = all_products)
+    all_products = products.find({}, {"name": 1, "price": 1, "quantity": 1})
+    return render_template("posDashboard.html", all_products=all_products)
+
+
+if __name__ == "__main__":
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(debug=True, threaded=True, port=8080)
